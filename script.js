275c06275ec75a9df39e5c56a6bc80b2740a6985
Lucas Gotz <tutigotz@gmail.com>
2022-05-01 17:10:27 -0300

    Modified calculator

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
-const currentValue = document.getElementById("current-value");
-const previousValue = document.getElementById("previous-value");
-const numberButtons = document.querySelectorAll(".number");
-const operatorButtons = document.querySelectorAll(".operator");
-
-const display = new Display(previousValue, currentValue);
+const numberButtons = document.querySelectorAll(".number")
+const operatorButtons = document.querySelectorAll(".operator")
+const currentDisplayNumber = document.querySelector("#current-value")
+const previousDisplayNumber = document.querySelector("#previous-value");
+let currentNumber = "";
+let previousNumber = "";
+let operator = "";
 
 numberButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        display.addNumber(button.innerHTML)
+    button.addEventListener('click', (e) => {
+        handleNumber(e.target.textContent)
     })
 })
 
+function handleNumber(number) { 
+    currentNumber += number
+    currentDisplayNumber.textContent = currentNumber
+
+}
+
 operatorButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        display.compute(button.value)
+    button.addEventListener('click', (e) => {
+        handleOperator(e.target.textContent)
     })
 })
+
+function handleOperator(op) {
+    operator = op;
+    if (op === "=") {
+        calculate()
+    }
+    previousNumber = currentNumber;
+    previousDisplayNumber.textContent = `${previousNumber} ${operator}`;
+    currentNumber = "";
+    currentDisplayNumber.textContent = ""
+}
+
+function calculate() {
+    previousNumber = Number(previousNumber);
+    currentNumber = Number(currentNumber);
+
+    if (operator === "+") {
+        previousNumber += currentNumber;
+    } else if (operator === "-") {
+        previousNumber -= currentNumber;
+    } else if (operator === "*") {
+        previousNumber *= currentNumber;
+    } else if (operator === "/") {
+        if (currentNumber <= 0) {
+            previousNumber = "MATH ERROR";
+        }
+        previousNumber /= currentNumber;
+    }
+
+    previousNumber = previousNumber.toString();
+    previousDisplayNumber.textContent = "";
+    currentDisplayNumber.textContent = previousNumber;
+    operator = "";
+}
+
+
 
 
