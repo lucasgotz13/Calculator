243533793cfc67c99b841843ceb4f54a05af1435
Lucas Gotz <tutigotz@gmail.com>
2022-05-01 21:32:05 -0300

    Fixed bugs and added functions over some buttons

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const numberButtons = document.querySelectorAll(".number")
 const operatorButtons = document.querySelectorAll(".operator")
 const currentDisplayNumber = document.querySelector("#current-value")
 const previousDisplayNumber = document.querySelector("#previous-value");
+const equalButton = document.querySelector(".equal");
 let currentNumber = "";
 let previousNumber = "";
 let operator = "";
@@ -12,7 +13,18 @@ numberButtons.forEach((button) => {
     })
 })
 
+equalButton.addEventListener('click', () => {
+    if (previousNumber !== "" && currentNumber !== "") {
+        calculate();
+    }
+})
+
 function handleNumber(number) { 
+    if (previousNumber !== "" && currentNumber !== "" && operator === "") {
+        previousNumber = "";
+        currentDisplayNumber.textContent = currentNumber;
+    } 
+    if (number === "." && currentNumber.includes(".")) return;
     currentNumber += number
     currentDisplayNumber.textContent = currentNumber
 
@@ -20,44 +32,87 @@ function handleNumber(number) {
 
 operatorButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
+        if (operator !== "") {
+            calculate()
+        }
         handleOperator(e.target.textContent)
     })
 })
 
-function handleOperator(op) {
-    operator = op;
-    if (op === "=") {
-        calculate()
+function handleOperator(op) { 
+    operator = op;  
+    if (previousNumber === "") {
+        previousNumber = currentNumber;
+        hasOperator(op)
+    } else if (currentNumber === "") {
+        hasOperator(op)
+    } else {
+        calculate();
+        operator = op;
+        currentDisplayNumber.textContent = ""
+        previousDisplayNumber.textContent = `${previousNumber} ${operator}`;
     }
-    previousNumber = currentNumber;
-    previousDisplayNumber.textContent = `${previousNumber} ${operator}`;
+}
+
+function hasOperator(text) {
+    operator = text;
+    previousDisplayNumber.textContent = `${previousNumber} ${operator}`
+    currentDisplayNumber.textContent = "";
     currentNumber = "";
-    currentDisplayNumber.textContent = ""
 }
 
 function calculate() {
     previousNumber = Number(previousNumber);
     currentNumber = Number(currentNumber);
-
+    
     if (operator === "+") {
         previousNumber += currentNumber;
+        console.log(previousNumber)
     } else if (operator === "-") {
         previousNumber -= currentNumber;
     } else if (operator === "*") {
         previousNumber *= currentNumber;
     } else if (operator === "/") {
         if (currentNumber <= 0) {
-            previousNumber = "MATH ERROR";
+            previousNumber = "Math error";
+            displayResults(); 
+            return;
         }
         previousNumber /= currentNumber;
     }
-
     previousNumber = previousNumber.toString();
-    previousDisplayNumber.textContent = "";
-    currentDisplayNumber.textContent = previousNumber;
-    operator = "";
+    previousNumber = roundNumber(previousNumber);
+    displayResults() 
 }
 
+const clear = document.querySelector("#clear");
+clear.addEventListener('click', clearCalculator);
 
+function clearCalculator() {
+    previousNumber = "";
+    currentNumber = "";
+    operator = "";
+    currentDisplayNumber.textContent = "";
+    previousDisplayNumber.textContent = "";
+}
 
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000;
+}
 
+function displayResults() {
+    currentDisplayNumber.textContent = previousNumber;
+    previousDisplayNumber.textContent = "";
+    operator = "";
+    currentNumber = "";
+}
+
+const deleteButton = document.querySelector("#delete");
+deleteButton.addEventListener('click', deleteNumber)
+
+function deleteNumber() {
+    if (currentNumber !== "") {
+        currentNumber = currentNumber.slice(0,-1);
+        currentDisplayNumber.textContent = currentNumber;
+    }
+}
